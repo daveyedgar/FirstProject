f56246f483d84ea95aff33eed51cb11442a987b7
daveyedgar <62836024+daveyedgar@users.noreply.github.com>
2022-11-25 22:53:00 -0500

    updates

--- js/site.js
+++ js/site.js
@@ -2,7 +2,7 @@ let count = 0;
 
 function addTask() {
   count++;
-  let Tasktemplate = `<li  class="item  list-group-item">Task  ${count}</li>`;
+  let Tasktemplate = `<li  class="item  list-group-item">To Do  ${count}</li>`;
   let element = document.getElementById("list");
   element.innerHTML += Tasktemplate;
 }
